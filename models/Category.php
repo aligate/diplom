8035eb3e20b127bb18819a428311906a6e39f044
aligate <aligate26@gmail.com>
2017-04-25 18:31:06 +0200

    Update Category.php

--- models/Category.php
+++ models/Category.php
@@ -9,8 +9,8 @@ public function showCategoriesList(){
 							cat.category_id,
 							cat.title,
 							count(req.id) AS requests,
-                            				sum(is_published = '1') AS is_published,
-                            				sum(has_responce = '0') AS no_responce
+                            sum(is_published = '1') AS is_published,
+                            sum(has_responce = '0') AS no_responce
 							FROM request AS req RIGHT JOIN category AS cat ON req.cat_id = cat.category_id 
 							GROUP BY category_id");
 	$stmt->execute();
@@ -27,7 +27,7 @@ $stmt->execute(['title'=>$title]);
 public function delCatAndRequest($id){
 	
 	$stmt = $this->db->prepare("DELETE category, request, responce FROM category LEFT JOIN request ON request.cat_id=category.category_id LEFT JOIN responce ON responce.request_id= request.id WHERE category_id = :id");
-	$stmt->execute(['id'=>$id]);
+	$stmt->execute(['id'=> $id]);
 	
 }
 
@@ -39,47 +39,6 @@ public function showOneCat($id){
 	return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 }
-
-public function delRequest($id){
-	
-	$stmt = $this->db->prepare("DELETE FROM request WHERE id = {$id}");
-	$stmt ->execute();
-}
-
-public function hideRequest($id){
-	
-	$stmt = $this->db->prepare("UPDATE request SET is_published = 0 WHERE id = {$id}");
-	$stmt ->execute();
-}
-
-public function showRequest($id){
-	
-	$stmt = $this->db->prepare("UPDATE request SET is_published = 1 WHERE id = {$id}");
-	$stmt ->execute();
-}
-
-public function shiftRequest($cat, $id){
-	
-	$stmt = $this->db->prepare("UPDATE request SET cat_id = {$cat} WHERE id = {$id}");
-	$stmt ->execute();
-}
-
-public function setResponce($id, $text){
-	
-	$stmt = $this->db->prepare("INSERT INTO responce (request_id, text) VALUES ({$id}, {$text})");
-	$stmt->execute();
-	
-}
-
-public function showNewRequest($cat){
-	
-	$stmt = $this->db->prepare("SELECT *
-    FROM request WHERE has_responce=0 AND cat_id= {$cat} ORDER BY id");
-	$stmt->execute();
-	return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-}
-
 
 }
 
