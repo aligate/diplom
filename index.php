f488814f97e5cb8401c59ca60f002573b6a454e6
aligate <aligate26@gmail.com>
2017-04-27 12:54:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,6 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
 ini_set('display_errors', 1);
 session_start();
 
-$config = include 'config.php';
-
-/**
- * Подключение к базе данных
- */
-include 'lib/DataBase.php';
-
-$db = DataBase::getDbConnection(
-	$config['mysql']['host'],
-	$config['mysql']['dbname'],
-	$config['mysql']['user'],
-	$config['mysql']['pass']
-);
-
 include 'lib/router.php';
 
+?>
