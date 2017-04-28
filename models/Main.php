9c8caac6fc396ecc0a2ce3e55843cb7905bbc73e
aligate <aligate26@gmail.com>
2017-04-28 11:40:23 +0200

    Update Main.php

--- models/Main.php
+++ models/Main.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once 'Model.php';
+namespace models;
 
 class Main extends Model
 {
 	
-	
-	// Получаем многомерный массив, где ключами будут имена тем, выводим на главной странице
-	
+	/**
+	* Получение всех всех вопросов
+	* @return array
+	*/
 	public function findAll()
 	{
 		$cat = $this->findAllСategories();
@@ -16,11 +17,10 @@ class Main extends Model
 							req.id,
 							req.text AS req_text,
 							res.text AS res_text
-	FROM category AS cat JOIN request AS req ON req.cat_id = cat.category_id JOIN responce AS res ON res.request_id = req.id 
-	WHERE req.is_published='1' ORDER BY cat.category_id");
+	FROM category AS cat JOIN request AS req ON req.cat_id = cat.category_id JOIN responce AS res ON res.request_id = req.id WHERE req.is_published='1' ORDER BY cat.category_id");
 		
 		if ($sth->execute()) {
-			$data =  $sth->fetchAll(PDO::FETCH_ASSOC);
+			$data =  $sth->fetchAll();
 			$all = [];
 			foreach($cat as $key => $item){
 				$key = $item['title'];
@@ -38,18 +38,50 @@ class Main extends Model
 	
 	function add($params)
 	{
-		$stmt = $this->db ->prepare("INSERT INTO request (text, cat_id, author, user_email) 
-		VALUES (:text, :cat_id, :author, :user_email);INSERT INTO responce (request_id, text) VALUES (LAST_INSERT_ID(), '')");
-		$stmt->bindParam('text', $params['text'], PDO::PARAM_STR);
-		$stmt->bindParam('cat_id', $params['category'], PDO::PARAM_STR);
-		$stmt->bindParam('author', $params['name'], PDO::PARAM_STR);
-		$stmt->bindParam('user_email', $params['email'], PDO::PARAM_STR);
+		$stmt = $this->db ->prepare("INSERT INTO request (text, cat_id, author, user_email) VALUES (:text, :cat_id, :author, :user_email);INSERT INTO responce (request_id, text) VALUES (LAST_INSERT_ID(), '')");
+		$stmt->bindParam('text', trim(addslashes($params['text'])), \PDO::PARAM_STR);
+		$stmt->bindParam('cat_id', $params['category'], \PDO::PARAM_STR);
+		$stmt->bindParam('author', trim(addslashes($params['name'])), \PDO::PARAM_STR);
+		$stmt->bindParam('user_email', trim(addslashes($params['email'])), \PDO::PARAM_STR);
 		
 		
 		return $stmt->execute();
 		
 	}
 	
+	
+	
+	// Авторизация
+	public function findAuth($log, $pass)
+	{
+		$stmt = $this->db->prepare("SELECT * FROM user WHERE login = :login AND password = :password");
+		
+		$stmt->execute(['login'=>$log, 'password'=>md5($pass)]);
+		
+		return $stmt->fetch(); 
+		
+		}
+	// Проверка на существование зарегистрированного логина	
+	public function checkLogin($login){
+		
+		$stmt = $this->db->prepare("SELECT * FROM user WHERE login = '{$login}'");
+		$stmt->execute();
+		if($stmt->rowCount() > 0){
+			return true;
+		}
+			return false;
+	}
+	// Регистрация
+	public function regist($login, $password){
+		
+	$stmt = $this->db->prepare("INSERT INTO user (login, password) VALUES (:login, :password)");
+	$stmt->execute(['login' =>$login, 'password' =>md5($password)]);
+	if($stmt->rowCount() === 1){
+		return true;
+	}
+	return false;
+	}
+
 	
 }
 
