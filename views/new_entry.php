da3312d61bbe205fd2c27028d4c6387f5a47fa4f
aligate <aligate26@gmail.com>
2017-04-28 12:17:51 +0200

    Update new_entry.php

--- views/new_entry.php
+++ views/new_entry.php
@@ -9,52 +9,62 @@
             <div class="breadcrumbs">
                 <ol class="breadcrumb">
                     <li><a href="?/category/list">Админпанель</a></li>
-                    <li class="active">Новые вопросы</li>
+                    <li class="active">Управление вопросами</li>
                 </ol>
             </div>
+			<div style="float: right"><a href="?/request/new" class="btn btn-default back"><i class="fa fa-plus"></i> Все новые вопросы</a></div>
 
-          
+           <div style="float: left">
+    <form action= "?/request/add/cat/{{entry.0.category_id}}" method="POST">
+       Новый вопрос: <input type="text" name="text" placeholder="Имя" value="" />
+        
+        <input type="submit" name="save" value="Создать вопрос" />
+    </form>
+</div>
        <br>     
        <br>
             
-            <h4>Список новых вопросов</h4>
+            <h4>Тема: {{entry.0.title}}</h4>
 
             <br/>
-			{%if newEntries == NULL%}
-					<p style="color: red">{{'Новых вопросов нет'}}</p>
-					{%else%}
 
             <table class="table-bordered table-striped table">
                 <tr>
                     
-                    <th>новые вопросы</th>
+                    <th>Текст вопроса</th>
                     <th>Дата</th>
-                    <th>Тема</th>
-					<th>Статус</th>
+                    <th>Автор</th>
+                    <th>Статус</th>
+		    <th>Публикация</th>
                     <th>Редактировать</th>
                     <th>Удалить</th>
                 </tr>
-				
-                {%for item in newEntries%}
-					
-						
+                {%for item in entry%}
+					{%if item.text==NULL%}
+					<p style="color: red">{{'В этой теме пока нет вопросов'}}</p>
+						{%else%}
                     <tr>
                        
-                        <td><a href="?/request/edit/cat/{{item.category_id}}/id/{{item.id}}">{{item.text}}</a></td>
+                        <td>{{item.text}}</td>
 						 <td>{{item.dated}}</td>
-						 <td>{{item.title}}</td>
+						 <td>{{item.author}}</td>
 							 {%if item.has_responce=='0'%}
                         <td><p style="color: red">{{'Ожидает ответа'}}</td>
-							
-							
+							{%else%}
+						 <td>{{'Дан ответ'}}</td>
+							 {%endif%}
+							{%if item.is_published=='1'%}
+							<td>{{'Опубликован'}}</td>
+								{%else%}
+                        <td>{{'Скрыт'}}</td>  
+							{%endif%}
                         <td><a href="?/request/edit/cat/{{item.category_id}}/id/{{item.id}}" title="Редактировать"><i class="fa fa-pencil-square-o"></i></a></td>
                         <td><a href="?/request/delete/cat/{{item.category_id}}/id/{{item.id}}" title="Удалить"><i class="fa fa-times"></i></a></td>
                     </tr>
 					{%endif%}
 					{%endfor%}
-					
             </table>
-            {%endif%}
+            
         </div>
     </div>
 </section>
