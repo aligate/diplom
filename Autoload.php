cae9f6bfd2dec19f0605ffdbb17a65c59af414e3
aligate <aligate26@gmail.com>
2017-04-28 14:17:07 +0200

    Update Autoload.php

--- Autoload.php
+++ Autoload.php
@@ -1,17 +1,6 @@
 <?php
 
 /**
- * Функция __autoload для автоматического подключения классов
+ * Функция __autoload
  */
-function __autoload($class_name)
-{
-    
-	// Формируем имя и путь к файлу с классом
-        $path = 'models/'. $class_name . '.php';
-
-        // Если такой файл существует, подключаем его
-        if (is_file($path)) {
-            include_once $path;
-        }
-    }
 
